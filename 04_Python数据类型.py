218410f77da0650fdb9a29f84ff50ea5b774adea
17680357643 <129883563+17680357643@users.noreply.github.com>
2023-09-25 20:33:25 +0800

    Add files via upload

---
+++ 04_Python数据类型.py
@@ -0,0 +1,15 @@
+# 数据类型
+print(type("黑马程序员"))
+print(type(666))
+print(type(11.345))
+
+string_type = type("黑马程序员")
+int_type = type(666)
+float_type = type(11.345)
+print(string_type)
+print(int_type)
+print(float_type)
+
+name = "黑马程序员"
+name_type = type(name)
+# print(name_type)
